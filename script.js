07114436e64ec69da6869e7fcde94cf99326886b
Sahil <sahilismailnavar@gmail.com>
2024-03-14 16:23:15 +0530

    Added songs fuction to the file

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Hello World");
 
 // fuction to get songs from directory 
 async function getSongs() {
-    let a = await fetch("url")
+    let a = await fetch("http://127.0.0.1:5500/songs/")
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -21,6 +21,11 @@ async function main() {
     // list of the songs 
     let songs = await getSongs();
     console.log(songs);
+
+    let songUl = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUl.innerHTML = songUl.innerHTML + `<li>song</li>`;
+    }
 
     // Play the first song 
     var audio = new Audio(songs[0]);
