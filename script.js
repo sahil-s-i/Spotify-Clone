43fb0480dae7a1f79c9f762d4627e9217595ab71
Sahil <sahilismailnavar@gmail.com>
2024-03-14 15:43:24 +0530

    Added songs fuction to the script.js

--- script.js
+++ script.js
@@ -17,11 +17,21 @@ async function getSongs() {
     return songs
 }
 
-async function main(){
+async function main() {
     // list of the songs 
     let songs = await getSongs();
     console.log(songs);
 
+    // Play the first song 
+    var audio = new Audio(songs[0]);
+    audio.play();
+
+    audio.addEventListener("loadeddata", () => {
+        let duration = audio.duration;
+        // The duration variable now holds the duration (in seconds) of the audio clip
+        console.log(duration);
+        console.log(audio.currentSrc, audio.currentTime);
+    });
 }
 
 
