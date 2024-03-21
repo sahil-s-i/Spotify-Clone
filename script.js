e999748a6f43ef014fe1826e511e5617d6f8838b
Sahil <sahilismailnavar@gmail.com>
2024-03-21 11:39:52 +0530

    Added event listner to the hamburger

--- script.js
+++ script.js
@@ -148,6 +148,11 @@ async function main() {
             currentSong.currentTime = ((currentSong.duration) * percent) / 100;
         })
 
+        // Addd an event listener for hamburger
+        document.querySelector(".hamburger").addEventListener("click", () => {
+            document.querySelector(".left").style.left = "0";
+        })
+
 
         // // Play the first song
         // if (songs.length > 0) {
