4dd80b18e790452cdef34d5015ea1d3097ca4500
Sahil <sahilismailnavar@gmail.com>
2024-03-22 12:03:26 +0530

    Edited song duration spaces properly

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ async function main() {
         // Listen for time update event 
         currentSong.addEventListener("timeupdate", () => {
             console.log(currentSong.currentTime, currentSong.duration);
-            document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`
+            document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`
             document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
         })
 
